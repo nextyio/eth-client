a164fcce6aefa45717a606641678bc2055217730
hadv <dvietha@gmail.com>
2018-09-20 11:29:25 +0700

    Reading data from file

--- main.go
+++ main.go
@@ -33,6 +33,10 @@ func main() {
 	for i, f := range files {
 		tos[i] = common.HexToAddress(f.Name()[37:])
 	}
+	data, err := ioutil.ReadFile("./data")
+	if err != nil {
+		log.Fatal(err)
+	}
 	for t := 0; t < len(tos); t++ {
 		client, err := ethclient.Dial("http://198.13.47.125:8545")
 		if err != nil {
@@ -45,7 +49,7 @@ func main() {
 		fmt.Println("NetworkID: ", networkID)
 		ks := keystore.NewKeyStore("./keystore", keystore.StandardScryptN, keystore.StandardScryptP)
 		to := tos[t]
-		value := big.NewInt(10000000000)
+		value := big.NewInt(10000000)
 		gasPrice, err := client.SuggestGasPrice(context.Background())
 		if err != nil {
 			fmt.Println(err.Error())
@@ -57,32 +61,7 @@ func main() {
 			go func(_from common.Address) {
 				defer wg.Done()
 				nonce, _ := client.PendingNonceAt(context.Background(), _from)
-				for i := 0; i < 2; i++ {
-					data := []byte(`When I handed my mother the green slip, she read the words back to me like she was returning borrowed things. 
-				She had kept her ugly close to her once, a toothbrush she returned to twice a day, a common ritual for the women with our faces. 
-				My mother knew she could smooth the ugly out of me as she did with her own ugly. 
-				She showed me pictures of Angela Davis, bought records with Diana on the sleeve, staring a hole through the ugly in me.
-				
-				My mother threw me these women, bones that called me to attention. 
-				But when I pressed my face against their faces, put my jaw to Angela’s jaw, 
-				I couldn’t make out the similarities, couldn’t find the same shine in my temples. 
-				I knew these women were supposed to remind me of myself, but there was an obvious disconnect between the Black girls presented to me as beautiful, 
-				and the Black girl written on the green slip.
-
-				When we talk about the importance of representation, we often start with children. 
-				Every child deserves to see a version of themselves on a screen—to point at, to call dibs on for a playground reenactment. 
-				Representation does the job of affirmation; it affirms the identity of the questioning child, the child in a predominantly white classroom, 
-				the child accustomed to different spaces.
-				
-				When I came home from school to watch TV, it would have been valuable to see a cartoon of a Black girl in the suburbs eating grits for breakfast. 
-				I would have cherished a sitcom with a Black girl with blue hair, if only as a wink and a nudge, a nod to my quiet existence.
-		
-				I was 18 when Black Panther came out, and like most Black Americans, I walked around with a glow. 
-				I had walked through my entire youth without a movie like it. 
-				I saw the movie for the first time in Atlanta, which made the whole experience Blacker than I thought possible. 
-				I wore my pride like an unconcealed weapon, walked with it until Twitter got quiet, until my mother stopped talking about it. 
-				The movie will always be a great example of the magic of representation, how it transfixes us.
-				`)
+				for i := 0; i < 1; i++ {
 					msg := ethereum.CallMsg{
 						From:     _from,
 						To:       &to,
